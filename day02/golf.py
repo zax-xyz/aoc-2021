578833b77799b60c122a6ab05ed14c8cabc5d805
Michael Vo <zax@zaxu.xyz>
2021-12-04 00:03:48 +1100

    refactor(day02/golf): manually split in loop instead of using map
    i kinda liked using map but this is shorter

--- day02/golf.py
+++ day02/golf.py
@@ -1,12 +1,12 @@
 # p1
 h=d=0
-for x,y in map(str.split,open('day2.txt')):z=int(y);a=len(x)-3;d+=z*a*(a<4);h+=z*(a>1)
+for b in open('day2.txt'):x,y=b.split();z=int(y);a=len(x)-3;d+=z*a*(a<4);h+=z*(a>1)
 print(h*d)
 
 # p2
 a=h=d=0
-for x,y in map(str.split,open('day2.txt')):
- z=int(y)
+for b in open('day2.txt'):
+ x,y=b.split();z=int(y)
  if'f'!=x[0]:a+=z*(len(x)-3)
  else:h+=z;d+=z*a
 print(h*d)
