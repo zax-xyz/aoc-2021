a498bded8f3467920e49758759c13b769007022b
Michael Vo <zax@zaxu.xyz>
2021-12-04 11:55:26 +1100

    refactor(day03/golf): flip the sum condition and swap c values to invert
    this saves one byte by using a strict inequality

--- day03/golf.py
+++ day03/golf.py
@@ -4,6 +4,6 @@ Based on original one-liner solution by "stap.le™wis" in UNSW CSESoc Discord:
 """
 
 # using strings
-print((y:=(lambda l,a,i=0:int(l[0],2)*int(a[0],2)if(len(l+a)<3)else y((b:=lambda l,c=0:[x for x in l if x[i]==(str(c^(sum(1-int(y[i])*2for y in l)<=0)))]if len(l)>1else l)(l),b(a,1),i+1)))(l:=[*open('day3.txt')],l))
+print((y:=(lambda l,a,i=0:int(l[0],2)*int(a[0],2)if(len(l+a)<3)else y((b:=lambda l,c=1:[x for x in l if x[i]==(str(c^(sum(1-int(y[i])*2for y in l)>0)))]if len(l)>1else l)(l),b(a,0),i+1)))(l:=[*open('day3.txt')],l))
 # using ints and bitwise operations (1 byte shorter)
-print((y:=(lambda l,a,i=2048:l[0]*a[0]if(len(l+a)<3)else y((b:=lambda l,c=0:[x for x in l if(x&i>0)==(c^(sum(i-(y&i)*2for y in l)<=0))]if len(l)>1else l)(l),b(a,1),i>>1)))(l:=[int(x,2)for x in open('day3.txt')],l))
+print((y:=(lambda l,a,i=2048:l[0]*a[0]if(len(l+a)<3)else y((b:=lambda l,c=1:[x for x in l if(x&i>0)==(c^(sum(i-(y&i)*2for y in l)>0))]if len(l)>1else l)(l),b(a,0),i>>1)))(l:=[int(x,2)for x in open('day3.txt')],l))
