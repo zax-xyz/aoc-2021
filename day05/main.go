f1bf3996bf479e090cede314ea586f9174678ea4
Michael Vo <zax@zaxu.xyz>
2021-12-05 23:50:11 +1100

    feat(day05): add part 2

--- day05/main.go
+++ day05/main.go
@@ -75,4 +75,47 @@ func parsePoints(line string) (from, to point) {
 }
 
 func p2() {
+	lines := lib.ReadLines("day5.txt")
+
+	grid := [1000][1000]int{}
+	dangerous := 0
+
+	for _, line := range lines {
+		from, to := parsePoints(line)
+		if from.x != to.x && from.y != to.y {
+			if from.x > to.x {
+				from, to = to, from
+			}
+
+			mul := 1
+			if from.y > to.y {
+				mul = -1
+			}
+
+			for i := 0; i <= to.x - from.x; i++ {
+				grid[from.x + i][from.y + i*mul]++
+				if grid[from.x + i][from.y + i*mul] == 2 {
+					dangerous++
+				}
+			}
+		} else if from.x != to.x {
+			min, max := minMax(from.x, to.x)
+			for x := min; x <= max; x++ {
+				grid[x][from.y]++
+				if grid[x][from.y] == 2 {
+					dangerous++
+				}
+			}
+		} else if from.y != to.y {
+			min, max := minMax(from.y, to.y)
+			for y := min; y <= max; y++ {
+				grid[from.x][y]++
+				if grid[from.x][y] == 2 {
+					dangerous++
+				}
+			}
+		}
+	}
+
+	fmt.Println(dangerous)
 }
