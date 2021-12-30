0111505451a0ba15fbbdaea94bf30a9897e8dc69
Michael Vo <zax@zaxu.xyz>
2021-12-31 00:24:40 +1100

    feat(day09): add part 2

--- day09/main.go
+++ day09/main.go
@@ -26,15 +26,64 @@ func p1() int {
 	return sum
 }
 
-func isLow(lines [][]int, row int, col int) bool {
+func isLow(lines [][]int, row, col int) bool {
 	height := lines[row][col]
-	return (
-		(row == len(lines) - 1 || height < lines[row + 1][col]) &&
-		(row == 0 || height < lines[row - 1][col]) &&
-		(col == len(lines[0]) - 1 || height < lines[row][col + 1]) &&
-		(col == 0 || height < lines[row][col - 1]))
+	return ((row == len(lines)-1 || height < lines[row+1][col]) &&
+		(row == 0 || height < lines[row-1][col]) &&
+		(col == len(lines[0])-1 || height < lines[row][col+1]) &&
+		(col == 0 || height < lines[row][col-1]))
 }
 
 func p2() int {
-	return 0
+	lines := lib.ReadInts("day9.txt")
+	biggestThree := [3]int{}
+
+	for i, line := range lines {
+		for j := range line {
+			size := dfs(lines, i, j)
+
+			min := biggestThree[0]
+			minI := 0
+			for i, n := range biggestThree {
+				if n < min {
+					min = n
+					minI = i
+				}
+			}
+
+			if size > min {
+				biggestThree[minI] = size
+			}
+		}
+	}
+
+	return biggestThree[0] * biggestThree[1] * biggestThree[2]
+}
+
+func dfs(lines [][]int, row, col int) int {
+	if lines[row][col] == 9 {
+		return 0
+	}
+
+	lines[row][col] = 9
+
+	count := 1
+	directions := [][2]int{
+		{-1, 0},
+		{0, 1},
+		{1, 0},
+		{0, -1},
+	}
+	for _, direction := range directions {
+		r := row + direction[0]
+		c := col + direction[1]
+		if r < 0 || r > len(lines)-1 ||
+			c < 0 || c > len(lines[0])-1 {
+			continue
+		}
+
+		count += dfs(lines, r, c)
+	}
+
+	return count
 }
